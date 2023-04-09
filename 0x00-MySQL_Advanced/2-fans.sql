b8cf74bdab988717908fab2ea494bb1c9b559ca7
saintbassanaga <saintbassanaga01@gmail.com>
2023-04-09 03:00:24 -0700

    update

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,5 @@
--- Select coloun into table fans.
--- render it as nb_fan and origin
-
-SELECT origin as origin , SUM(fans) as nb_fans FROM metal_bands
-
-GROUP BY origin
-
-ORDER BY nb_fans DESC
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans.
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
